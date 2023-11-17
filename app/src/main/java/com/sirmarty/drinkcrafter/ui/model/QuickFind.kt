1ad5d7bcd26839d6f4469d54bff228d5ab2a3b92
SirMarty <marti.tarres@gmail.com>
2023-11-17 12:05:26 +0100

    #quick_finds: added quick finds icons and strings

--- app/src/main/java/com/sirmarty/drinkcrafter/ui/model/QuickFind.kt
+++ app/src/main/java/com/sirmarty/drinkcrafter/ui/model/QuickFind.kt
@@ -1,11 +1,14 @@
 package com.sirmarty.drinkcrafter.ui.model
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import com.sirmarty.drinkcrafter.R
 
-enum class QuickFind(val ingredient: String, @DrawableRes val image: Int) {
-    Vodka("Vodka", R.drawable.ic_home),
-    Tequila("Tequila", R.drawable.ic_home),
-    Gin("Gin", R.drawable.ic_home),
-    Rum("Rum", R.drawable.ic_home),
+enum class QuickFind(val ingredient: String, @StringRes val text: Int, @DrawableRes val image: Int) {
+    Vodka("Vodka", R.string.quick_find_vodka, R.drawable.ic_vodka),
+    Tequila("Tequila", R.string.quick_find_tequila, R.drawable.ic_tequila),
+    Gin("Gin", R.string.quick_find_gin, R.drawable.ic_gin),
+    Rum("Rum", R.string.quick_find_rum, R.drawable.ic_rum),
+    Jagermeister("Jagermeister", R.string.quick_find_jager, R.drawable.ic_jagermeister),
+    Whiskey("Whiskey", R.string.quick_find_whiskey, R.drawable.ic_whiskey),
 }
--- app/src/main/java/com/sirmarty/drinkcrafter/ui/screens/categories/CategoriesScreen.kt
+++ app/src/main/java/com/sirmarty/drinkcrafter/ui/screens/categories/CategoriesScreen.kt
@@ -8,10 +8,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -19,7 +23,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,6 +34,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -63,12 +67,14 @@ fun CategoriesScreen(
                     modifier = Modifier.align(Alignment.Center)
                 )
             }
+
             UiState.Loading -> {
                 CircularProgressIndicator(Modifier.align(Alignment.Center))
             }
+
             is UiState.Success -> {
                 Column {
-                    QuickFinds(onQuickFindClick)
+                    QuickFinds(context, onQuickFindClick)
                     CategoryList(context, (uiState as UiState.Success).value, onCategoryClick)
                 }
             }
@@ -77,13 +83,32 @@ fun CategoriesScreen(
 }
 
 @Composable
-fun QuickFinds(onQuickFindClick: (String) -> Unit) {
+fun QuickFinds(context: Context, onQuickFindClick: (String) -> Unit) {
     val quickFinds = QuickFind.values()
-    LazyRow(Modifier.fillMaxWidth().padding(16.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+    LazyRow(
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
         items(quickFinds) {
-            Column(Modifier.clickable { onQuickFindClick(it.ingredient) }) {
-                Icon(painterResource(it.image), contentDescription = "")
-                Text(text = it.ingredient)
+            Column(
+                Modifier
+                    .width(80.dp)
+                    .clickable { onQuickFindClick(it.ingredient) },
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painterResource(it.image),
+                    contentDescription = "",
+                    modifier = Modifier.size(64.dp)
+                )
+                Spacer(Modifier.height(8.dp))
+                Text(
+                    text = context.getString(it.text),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
@@ -91,7 +116,11 @@ fun QuickFinds(onQuickFindClick: (String) -> Unit) {
 
 
 @Composable
-fun CategoryList(context: Context, categories: List<CategoryWithImage>, onCategoryClick: (String) -> Unit) {
+fun CategoryList(
+    context: Context,
+    categories: List<CategoryWithImage>,
+    onCategoryClick: (String) -> Unit
+) {
     LazyColumn(
         modifier = Modifier.fillMaxWidth(),
         contentPadding = PaddingValues(16.dp),
