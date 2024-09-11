d306e68228f38884a7e21fd152d2563d82cef4eb
SirMarty <marti.tarres@gmail.com>
2024-09-11 17:10:04 +0200

    #custom_font: changed default font for a custom one in material theme

--- app/src/main/java/com/sirmarty/drinkcrafter/ui/theme/Type.kt
+++ app/src/main/java/com/sirmarty/drinkcrafter/ui/theme/Type.kt
@@ -2,14 +2,27 @@ package com.sirmarty.drinkcrafter.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sirmarty.drinkcrafter.R
+
+// Define the custom font family
+val drinkCrafterFontFamily = FontFamily(
+    Font(R.font.dosis_extra_light, FontWeight.ExtraLight),
+    Font(R.font.dosis_light, FontWeight.Light),
+    Font(R.font.dosis_regular, FontWeight.Normal),
+    Font(R.font.dosis_medium, FontWeight.Medium),
+    Font(R.font.dosis_semi_bold, FontWeight.SemiBold),
+    Font(R.font.dosis_bold, FontWeight.Bold),
+    Font(R.font.dosis_extra_bold, FontWeight.ExtraBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = drinkCrafterFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
