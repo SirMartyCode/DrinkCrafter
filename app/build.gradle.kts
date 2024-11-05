90ef5c71532dd5000071f253dbec2ac56a4c570f
SirMartyCode <marti.tarres@gmail.com>
2024-11-05 12:09:15 +0100

    #release_pro: version number updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.sirmarty.drinkcrafter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
